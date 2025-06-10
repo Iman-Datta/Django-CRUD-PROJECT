8a1c437ae878b74a4dc3050f8f9012f7a02480db
Iman-Datta <dattaiman56@gmail.com>
2025-06-10 21:32:41 +0530

    add

--- crud/crud/urls.py
+++ crud/crud/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home)
+    path('',views.home, name = '_home'),
+    path('add', views.add, name='_add')
 ]
--- crud/crud/views.py
+++ crud/crud/views.py
@@ -1,5 +1,16 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+def home(request: HttpRequest):
+    if request.method == 'GET':
+        msg: str = 'Welcome to Home'
+        return render(request,"index.html", {'message': msg})
 
-def home(request):
-    return render(request,"index.html")
+def add(request: HttpRequest):
+    if request.method == 'POST':
+        fNum: int = int(request.POST['firstNumber'])
+        sNum: int = int(request.POST['secondNumber'])
+        print(fNum+sNum)
+        return render(request,"create.html")
+    
+    return render(request,"create.html")
